f9f248798ddb16528a73464a2e723f3e2229135a
cso1992 <csoon1992@gmail.com>
2016-10-09 23:00:09 +0200

    styling

--- resources/assets/js/ui/user/loginform.jsx
+++ resources/assets/js/ui/user/loginform.jsx
@@ -4,7 +4,7 @@ import Register from './registerform.jsx';
 
 export default class Login extends React.Component {
 	render() {
-		return <Segment vertical>
+		return <Segment vertical className="login-form">
 			<Button color="facebook" href="/redirect">
 				<Icon name="facebook" />
 				Entrar con Facebook
