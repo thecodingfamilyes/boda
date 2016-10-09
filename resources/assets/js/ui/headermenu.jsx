ba1584b130ec077bada1dea4ff004ed7309bb6a2
algm <piticonejo@gmail.com>
2016-10-09 20:41:41 +0200

    Removing asistentes from header

--- resources/assets/js/ui/headermenu.jsx
+++ resources/assets/js/ui/headermenu.jsx
@@ -72,7 +72,7 @@ export default class HeaderMenu extends React.Component {
 								<Dropdown.Item name="La boda" active={activePath == '/boda'}><Link to="/boda">La Boda</Link></Dropdown.Item>
 								<Dropdown.Item name="Libro de firmas" active={activePath == '/firmas'}> <Link to="/firmas">Libro de firmas</Link> </Dropdown.Item>
 								<Dropdown.Item name="Pregúntanos" active={activePath == '/faq'}> <Link to="/faq">Pregúntanos</Link> </Dropdown.Item>
-								<Dropdown.Item name="Asistentes" active={activePath == '/asistentes'}> <Link to="/asistentes">Asistentes</Link> </Dropdown.Item>
+
 							</Dropdown.Menu>
 						</Dropdown>
 					</Menu.Item>
@@ -95,7 +95,7 @@ export default class HeaderMenu extends React.Component {
 							<Menu.Item name="La boda" active={activePath == '/boda'}><Link to="/boda">La Boda</Link></Menu.Item>
 							<Menu.Item name="Libro de firmas" active={activePath == '/firmas'}> <Link to="/firmas">Libro de firmas</Link> </Menu.Item>
 							<Menu.Item name="Pregúntanos" active={activePath == '/faq'}> <Link to="/faq">Pregúntanos</Link> </Menu.Item>
-							<Menu.Item name="Asistentes" active={activePath == '/asistentes'}> <Link to="/asistentes">Asistentes</Link> </Menu.Item>
+
 						</Menu.Menu>
 						<Menu.Menu position="right">
 							<Menu.Item>
@@ -116,7 +116,7 @@ export default class HeaderMenu extends React.Component {
 						<Menu.Item name="La boda" active={activePath == '/boda'}><Link to="/boda">La Boda</Link></Menu.Item>
 						<Menu.Item name="Libro de firmas" active={activePath == '/firmas'}> <Link to="/firmas">Libro de firmas</Link> </Menu.Item>
 						<Menu.Item name="Pregúntanos" active={activePath == '/faq'}> <Link to="/faq">Pregúntanos</Link> </Menu.Item>
-						<Menu.Item name="Asistentes" active={activePath == '/asistentes'}> <Link to="/asistentes">Asistentes</Link> </Menu.Item>
+
 					</Menu.Menu>
 					<Menu.Menu position="right">
 						<Menu.Item>
