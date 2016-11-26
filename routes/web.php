118c8e4bdeaeaab63709bf73e83bc89f45709268
algm <piticonejo@gmail.com>
2016-11-26 14:15:38 +0100

    adding asistencia route behavior

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Transformers\UsersTransformer;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,22 +16,25 @@ use App\Transformers\UsersTransformer;
 require_once 'api.php';
 
 if (!function_exists('loadHome')) {
+
 	function loadHome() {
-		$me = Auth::user();
+		$me = app('auth')->user();
 
 		if (!is_null($me)) {
 			$transformer = new UsersTransformer();
 			$token = $me->api_token;
 			$me = json_encode(array_merge($transformer->transform($me), compact('token')), JSON_NUMERIC_CHECK);
-		} else {
-			$me = 'null';
 		}
 
 		return view('home')->with('me', $me);
 	}
 }
 
-Route::get('/', function () {
+Route::get('/', function (Request $request) {
+	if ($request->input('code') === 'yovoy') {
+		return redirect('/asistencia');
+	}
+
 	return loadHome();
 });
 
